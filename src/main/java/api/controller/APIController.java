cc9f99fb87d4bdee4fbf1c8a1c0a268c51e8acff
MinhNT <minhnt@peacesoft.net>
2016-12-27 14:59:18 +0700

    api with params

--- src/main/java/api/controller/APIController.java
+++ src/main/java/api/controller/APIController.java
@@ -1,6 +1,7 @@
 package api.controller;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -10,10 +11,10 @@ import api.entity.Student;
 @Path("/api")
 public class APIController {
 	
-	@GET
+	@POST
 	@Path("/test")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Student getStudent(){
+	public Student getStudent(Student student){
 		
 		return new Student();
 	}
