c7cc87812e289c1ecb780093d84737a45533bed8
MinhNT <minhnt@peacesoft.net>
2016-12-27 17:28:12 +0700

    servlet api

--- src/main/java/api/controller/APIController.java
+++ src/main/java/api/controller/APIController.java
@@ -1,9 +1,11 @@
 package api.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import api.entity.Student;
@@ -14,7 +16,7 @@ public class APIController {
 	@POST
 	@Path("/test")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Student getStudent(Student student){
+	public Student getStudent(Student student,@Context HttpServletRequest http){
 		
 		return new Student();
 	}
